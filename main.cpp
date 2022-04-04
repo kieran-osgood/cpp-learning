06fb6fa5af529c3c20500cd2bc8f039511e417d2
Kieran Osgood <kieran.osgood@and.digital>
2022-04-04 11:22:35 +0100

    feat: move models into sources

--- main.cpp
+++ main.cpp
@@ -1,49 +1,24 @@
 #include <iostream>
-#include <unistd.h>
-#include <term.h>
-//#include <utility>
-//#include "FlyingCar.h"
+#include "sources/Car/Car.h"
+//#include <term.h>
 
-template<typename T>
-bool checkGuess(T p1, T p2) {
-    return p1 == p2;
-}
-
-void ClearScreen() {
-    if (!cur_term) {
-        int result;
-        setupterm(NULL, STDOUT_FILENO, &result);
-        if (result <= 0) return;
-    }
-
-    putp(tigetstr("clear"));
-}
+//void ClearScreen() {
+//    if (!cur_term) {
+//        int result;
+//        setupterm(NULL, STDOUT_FILENO, &result);
+//        if (result <= 0) return;
+//    }
+//
+//    putp(tigetstr("clear"));
+//}
 
 int main() {
-    int p1 = 0, p2 = 0, attempts = 0;
-    std::cout << "p1 guess:";
-    std::cin >> p1;
-
-    do {
-        ClearScreen();
-        std::cout << "p2 guess:";
-        std::cin >> p2;
-
-        if (checkGuess(p1, p2)) {
-            std::cout << "Correct!" << std::endl;
-            break;
-        }
-
-        attempts++;
-    } while (attempts < 3);
-
-    if (attempts == 3) {
-        std::cout << "Failed!" << std::endl;
-    }
+    std::cout << "Hello World";
+    Car myCar("Super fast");
+    myCar.move();
 
     system("read -p 'Press Enter to continue...' var");
 }
-
 
 //int main() {
 //    FlyingCar flyingCar("Volvo");
---
+++ sources/Factorial/FactorialTest.h
@@ -0,0 +1,10 @@
+//
+// Created by Kieran Osgood on 31/03/2022.
+//
+
+#ifndef UNTITLED_FACTORIALTEST_H
+#define UNTITLED_FACTORIALTEST_H
+
+int Factorial(int n);  // Returns the factorial of n
+
+#endif //UNTITLED_FACTORIALTEST_H
