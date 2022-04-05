61b8178c5515b9c5e760600a66660790ed44bd40
Kieran Osgood <kieran.osgood@and.digital>
2022-04-05 17:32:52 +0100

    feat: remove submodule built gtest for FetchContent inclusion

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,6 +6,6 @@ int main() {
     std::cout << "Hello World" << std::endl;
     Car myCar("Super fast");
     myCar.move();
-    std::cout << std::endl << "factorial of 1=" << Factorial().calculate(1);
+    std::cout << std::endl << "factorial of 9=" << Factorial().calculate(9);
 //    system("read -p 'Press Enter to continue...' var");
 }
